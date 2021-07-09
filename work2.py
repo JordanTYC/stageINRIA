929c507f402e2a0cf9a92fb9d3792fcd74f49b23
JordanTYC <jordan.tayac@gmail.com>
2021-07-09 17:52:36 +0200

    class variant removed

--- work2.py
+++ work2.py
@@ -5,78 +5,13 @@
     Comparison of the number of isolated barcodes between real variants and false one.
 """
 
+
 import argparse, pysam, xlsxwriter
 from statistics import mean
-
+from Variant import Variant
 
 N_GAP = 5000     # space allowed between linked-reads in cluster
 L_SV = [2000,10000] # lengths for variants
-
-
-class Variant:
-    '''
-        Splits a string (vcf format) to create a variant.
-        
-        line -- string
-    '''
-    def __init__(self,line):
-        line = line.split()
-        self.info = self.createDict(line)
-        self.chrom = line[0]
-        self.pos = self.get_pos(line)
-        self.id = line[2]
-        self.ref = line[3]
-        self.alt = line[4]
-        self.qual = line[5]
-        self.filter = line[6]
-        
-    def createDict(self,line):
-        '''
-            Creates a dictionnary via all the info.
-        '''
-        info = line[7].split(";")
-        d = {}
-        for field in info:
-            e = field.split('=')
-            if len(e) == 1: # exception for keywords without value
-                d[e[0]] = ""
-            else:
-                d[e[0]] = e[1] 
-        return d
-        
-    def get_pos(self,line):
-        '''
-            Returns the variant's start position.
-        '''
-        if self.get_svtype() == "INS" and "LEFT_SVINSSEQ" in self.info:
-            return int(line[1]) - len(self.info["LEFT_SVINSSEQ"])
-        return int(line[1])
-
-    def get_end(self):
-        '''
-            Returns the variant's end position.
-        '''
-        svtype = self.get_svtype()
-        if svtype == "INS":
-            if "RIGHT_SVINSSEQ" not in self.info:
-                return self.pos + int(self.info["SVLEN"])
-            else:
-                return self.pos + len(self.info["RIGHT_SVINSSEQ"])
-        else:
-            return int(self.info["END"])
-
-    def get_svtype(self):
-        '''
-            Returns the variant's type.
-        '''
-        return self.info["SVTYPE"]
-    
-    def get_svlen(self):
-        '''
-            Returns variant's length.
-        '''
-        if "SVLEN" in self.info:
-            return int(self.info["SVLEN"])
 
 
 def trueSV(file):
@@ -98,7 +33,7 @@ def trueSV(file):
                 truth.append((sv[2],int(sv[3]),int(sv2[3])))
             else:
                 truth.append((sv[0],int(sv[1]),int(sv[3])))
-        line = filin.readline()
+            line = filin.readline()
     return truth
 
     
@@ -264,10 +199,11 @@ def clean_P(P):
         P -- list from partition()
     '''
     # calculation of the mean :
-    M = []
-    for [a,b,c] in P:
-        M.append(c)
-    n = mean(M)
+    #M = []
+    #for [a,b,c] in P:
+    #    M.append(c)
+    #n = mean(M)
+    n = 6
     # removes short clusters :
     F = []
     for [a,b,c] in P:
@@ -305,14 +241,14 @@ def nb_isolated(L,bci,D,c):
         D -- dict resulting from store_bx()
     '''
     cpt = 0
-    with open("partitions.txt","a") as test:
-        for (bx,pos) in L:
-            P = partition(D,bx,c)
-            T = forTest(P)
-            test.write("\n"+str(T))
-            P = clean_P(P)
-            if isIsolated(pos,P):
-                cpt += 1
+    #with open("partitions.txt","a") as test:
+    for (bx,pos) in L:
+        P = partition(D,bx,c)
+        #T = forTest(P)
+        #test.write("\n"+str(T))
+        P = clean_P(P)
+        if isIsolated(pos,P):
+            cpt += 1
     return cpt
 
 
