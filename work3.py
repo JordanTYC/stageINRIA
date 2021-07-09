ccee204c3fe663f8a2bad6b035344bed715240db
JordanTYC <jordan.tayac@gmail.com>
2021-07-09 17:53:43 +0200

    class Variant removed

--- work3.py
+++ work3.py
@@ -5,75 +5,14 @@
     Comparison of the number of common barcodes between real variants and false one.
 """
 
+
 import argparse, pysam, xlsxwriter
+from Variant import Variant
 
 GAP = 500
 L_SV = [2000,10000] # lengths for variants
 
-class Variant:
-    '''
-        Splits a string (vcf format) to create a variant.
-        
-        line -- string
-    '''
-    def __init__(self,line):
-        line = line.split()
-        self.info = self.createDict(line)
-        self.chrom = line[0]
-        self.pos = self.get_pos(line)
-        self.id = line[2]
-        self.ref = line[3]
-        self.alt = line[4]
-        self.qual = line[5]
-        self.filter = line[6]
-        
-    def createDict(self,line):
-        '''
-            Creates a dictionnary via all the info.
-        '''
-        info = line[7].split(";")
-        d = {}
-        for field in info:
-            e = field.split('=')
-            if len(e) == 1: # exception for keywords without value
-                d[e[0]] = ""
-            else:
-                d[e[0]] = e[1] 
-        return d
-        
-    def get_pos(self,line):
-        '''
-            Returns the variant's start position.
-        '''
-        if self.get_svtype() == "INS" and "LEFT_SVINSSEQ" in self.info:
-            return int(line[1]) - len(self.info["LEFT_SVINSSEQ"])
-        return int(line[1])
 
-    def get_end(self):
-        '''
-            Returns the variant's end position.
-        '''
-        svtype = self.get_svtype()
-        if svtype == "INS":
-            if "RIGHT_SVINSSEQ" not in self.info:
-                return self.pos + int(self.info["SVLEN"])
-            else:
-                return self.pos + len(self.info["RIGHT_SVINSSEQ"])
-        else:
-            return int(self.info["END"])
-
-    def get_svtype(self):
-        '''
-            Returns the variant's type.
-        '''
-        return self.info["SVTYPE"]
-    
-    def get_svlen(self):
-        '''
-            Returns variant's length.
-        '''
-        if "SVLEN" in self.info:
-            return int(self.info["SVLEN"])
 
 def trueSV(file):
     '''
@@ -94,7 +33,7 @@ def trueSV(file):
                 truth.append((sv[2],int(sv[3]),int(sv2[3])))
             else:
                 truth.append((sv[0],int(sv[1]),int(sv[3])))
-        line = filin.readline()
+            line = filin.readline()
     return truth
     
 
@@ -203,6 +142,7 @@ def sortSV(vcf,bam,truth,margin):
         truth -- file with real variants
         margin -- boolean
     '''
+    cpt = 1
     L = []
     row = 15 * [0]
     m = 100 if margin else 0
@@ -221,6 +161,8 @@ def sortSV(vcf,bam,truth,margin):
         # for each variant :
         while line != '':
             v = Variant(line)
+            print("variant",cpt)
+            cpt += 1
             # We keep filling L if both chromosomes correspond to current one
             # If not, this means we're not processing the same variant anymore, so we treat the BND we've read so far
             if v.get_svtype() == "BND" and ((curChr1 == "" and curChr2 == "") or (curChr1 == v.chrom and curChr2 == get_chrom_bnd(v))):
